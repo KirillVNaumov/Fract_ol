0f7391f1f1518c1951d22dee3f6cd597518921a4
Kirill Naumov <knaumov@e1z2r7p41.42.us.org>
2019-06-05 21:14:34 -0700

    123

--- srcs/fractol/create_palette.c
+++ srcs/fractol/create_palette.c
@@ -1,70 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_palette.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knaumov <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/05 21:10:30 by knaumov           #+#    #+#             */
+/*   Updated: 2019/06/05 21:10:31 by knaumov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fractol.h"
 
-void        create_psychedelic(t_fractol *fractol)
+void		assign_psychedelic_continue(t_fractol *fractol)
 {
-    int     i;
-
-    i = 0;
-    fractol->psychedelic_palette = (t_color **)malloc(sizeof(t_color *) * 11);
-    fractol->psychedelic_palette[10] = NULL;
-    while (i < 10)
-        fractol->psychedelic_palette[i++] = (t_color *)malloc(sizeof(t_color));
-    fractol->psychedelic_palette[0]->red = 41;
-    fractol->psychedelic_palette[0]->green = 44;
-    fractol->psychedelic_palette[0]->blue = 232;
-
-    fractol->psychedelic_palette[1]->red = 242;
-    fractol->psychedelic_palette[1]->green = 187;
-    fractol->psychedelic_palette[1]->blue = 7;
-
-    fractol->psychedelic_palette[2]->red = 175;
-    fractol->psychedelic_palette[2]->green = 7;
-    fractol->psychedelic_palette[2]->blue = 242;
-
-    fractol->psychedelic_palette[3]->red = 52;
-    fractol->psychedelic_palette[3]->green = 242;
-    fractol->psychedelic_palette[3]->blue = 7;
-
-    fractol->psychedelic_palette[4]->red = 242;
-    fractol->psychedelic_palette[4]->green = 7;
-    fractol->psychedelic_palette[4]->blue = 7;
-
-    fractol->psychedelic_palette[5]->red = 33;
-    fractol->psychedelic_palette[5]->green = 6;
-    fractol->psychedelic_palette[5]->blue = 165;
-
-    fractol->psychedelic_palette[6]->red = 244;
-    fractol->psychedelic_palette[6]->green = 239;
-    fractol->psychedelic_palette[6]->blue = 100;
-    
-    fractol->psychedelic_palette[7]->red = 174;
-    fractol->psychedelic_palette[7]->green = 95;
-    fractol->psychedelic_palette[7]->blue = 175;
-    
-    fractol->psychedelic_palette[8]->red = 21;
-    fractol->psychedelic_palette[8]->green = 124;
-    fractol->psychedelic_palette[8]->blue = 77;
-        
-    fractol->psychedelic_palette[9]->red = 219;
-    fractol->psychedelic_palette[9]->green = 139;
-    fractol->psychedelic_palette[9]->blue = 10;
+	fractol->psychedelic_palette[5]->red = 33;
+	fractol->psychedelic_palette[5]->green = 6;
+	fractol->psychedelic_palette[5]->blue = 165;
+	fractol->psychedelic_palette[6]->red = 244;
+	fractol->psychedelic_palette[6]->green = 239;
+	fractol->psychedelic_palette[6]->blue = 100;
+	fractol->psychedelic_palette[7]->red = 174;
+	fractol->psychedelic_palette[7]->green = 95;
+	fractol->psychedelic_palette[7]->blue = 175;
+	fractol->psychedelic_palette[8]->red = 21;
+	fractol->psychedelic_palette[8]->green = 124;
+	fractol->psychedelic_palette[8]->blue = 77;
+	fractol->psychedelic_palette[9]->red = 219;
+	fractol->psychedelic_palette[9]->green = 139;
+	fractol->psychedelic_palette[9]->blue = 10;
 }
 
-void        create_palette(t_fractol *fractol)
+void		assign_psychedelic(t_fractol *fractol)
 {
-    fractol->palette = new_palette();
-    fractol->palette->color->green = 100;
-    fractol->palette->color->blue = 180;
-    fractol->palette->next = new_palette();
-    fractol->palette->next->color->red = 150;
-    fractol->palette->next->color->blue = 150;
-    fractol->palette->next->color->green = 150;
-    fractol->palette->next->next = new_palette();
-    fractol->palette->next->next->color->green = 100;
-    fractol->palette->next->next->color->red = 130;
-    fractol->palette->next->next->next = new_palette();
-    fractol->palette->next->next->next->color->blue = 180;
-    fractol->palette->next->next->next->color->red = 20;
-    fractol->palette->next->next->next->next = fractol->palette;
-    create_psychedelic(fractol);
+	fractol->psychedelic_palette[0]->red = 41;
+	fractol->psychedelic_palette[0]->green = 44;
+	fractol->psychedelic_palette[0]->blue = 232;
+	fractol->psychedelic_palette[1]->red = 242;
+	fractol->psychedelic_palette[1]->green = 187;
+	fractol->psychedelic_palette[1]->blue = 7;
+	fractol->psychedelic_palette[2]->red = 175;
+	fractol->psychedelic_palette[2]->green = 7;
+	fractol->psychedelic_palette[2]->blue = 242;
+	fractol->psychedelic_palette[3]->red = 52;
+	fractol->psychedelic_palette[3]->green = 242;
+	fractol->psychedelic_palette[3]->blue = 7;
+	fractol->psychedelic_palette[4]->red = 242;
+	fractol->psychedelic_palette[4]->green = 7;
+	fractol->psychedelic_palette[4]->blue = 7;
+	assign_psychedelic_continue(fractol);
+}
+
+void		create_psychedelic(t_fractol *fractol)
+{
+	int		i;
+
+	i = 0;
+	fractol->psychedelic_palette = (t_color **)malloc(sizeof(t_color *) * 11);
+	fractol->psychedelic_palette[10] = NULL;
+	while (i < 10)
+		fractol->psychedelic_palette[i++] = (t_color *)malloc(sizeof(t_color));
+	assign_psychedelic(fractol);
+}
+
+void		create_palette(t_fractol *fractol)
+{
+	fractol->palette = new_palette();
+	fractol->palette->color->green = 100;
+	fractol->palette->color->blue = 180;
+	fractol->palette->next = new_palette();
+	fractol->palette->next->color->red = 150;
+	fractol->palette->next->color->blue = 150;
+	fractol->palette->next->color->green = 150;
+	fractol->palette->next->next = new_palette();
+	fractol->palette->next->next->color->green = 100;
+	fractol->palette->next->next->color->red = 130;
+	fractol->palette->next->next->next = new_palette();
+	fractol->palette->next->next->next->color->blue = 180;
+	fractol->palette->next->next->next->color->red = 20;
+	fractol->palette->next->next->next->next = fractol->palette;
+	create_psychedelic(fractol);
 }
--- srcs/newton_function_generator/parse_input.c
+++ srcs/newton_function_generator/parse_input.c
@@ -1,74 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_input.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knaumov <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/05 20:57:09 by knaumov           #+#    #+#             */
+/*   Updated: 2019/06/05 20:57:10 by knaumov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "calc.h"
 
-void            parse_function(char *function, t_calc *info)
+int				identify_power(char **function)
 {
-    int     coefficient;
-    int     power;
+	int			power;
 
-    while (*function)
-    {
-        coefficient = ft_atoi(function);
-        if (coefficient == 0)
-            coefficient = (*function == '-') ? -1 : 1;
-        if (*function == '+' || *function == '-')
-            ++function;
-        while (*function >= '0' && *function <= '9')
-            ++function;
-        if (*function != 'x')
-            power = 0;
-        else
-        {
-            ++function;
-            if (*function != '^')
-                power = 1;
-            else
-            {
-                ++function;
-                power = ft_atoi(function);
-                while (*function >= '0' && *function <= '9')
-                    ++function;
-            }
-        }
-        info->function = add_summand(info->function, coefficient, power);
-    }
+	if (**function != '^')
+		power = 1;
+	else
+	{
+		++(*function);
+		power = ft_atoi(*function);
+		while (**function >= '0' && **function <= '9')
+			++(*function);
+	}
+	return (power);
 }
 
-void            parse_input(char *function, t_calc *info)
+void			parse_function(char *function, t_calc *info)
 {
-    int     i;
+	int		coefficient;
+	int		power;
 
-    if (ft_strchr(function, 'x') == NULL)
-        error("The function is written in incorrect form");
-    i = -1;
-    while (function[++i])
-        if (!((function[i] >= '0' && function[i] <= '9') || \
-            function[i] == '+' || function[i] == '-' || \
-            function[i] == '^' || function[i] == 'x'))
-            error("The function caontains unrecognized symbols");
-    i = 0;
-    while (function[i])
-    {
-        if (function[i] == '+' || function[i] == '-')
-            ++i;
-        if (function[i] == '+' || function[i] == '-' || function[i] == '\0')
-            error("Parsing error");
-        while (function[i] >= '0' && function[i] <= '9')
-            ++i;
-        if (function[i] == 'x')
-        {
-            ++i;
-            if (function[i] == '^')
-            {
-                ++i;
-                if (function[i] < '0' || function[i] > '9')
-                    error("Parsing error");
-                while (function[i] >= '0' && function[i] <= '9')
-                    ++i;   
-            }
-        }
-        if (function[i] < '0' || function[i] > '9')
-            if (function[i] != '-' && function[i] != '+' && function[i] != '\0')
-                error("Parsing error");
-    }
-    parse_function(function, info);
+	while (*function)
+	{
+		coefficient = ft_atoi(function);
+		if (coefficient == 0)
+			coefficient = (*function == '-') ? -1 : 1;
+		if (*function == '+' || *function == '-')
+			++function;
+		while (*function >= '0' && *function <= '9')
+			++function;
+		if (*function != 'x')
+			power = 0;
+		else
+		{
+			++function;
+			power = identify_power(&function);
+		}
+		info->function = add_summand(info->function, coefficient, power);
+	}
+}
+
+int				iterate_with_index(char *function, int i)
+{
+	if (function[i] == '+' || function[i] == '-')
+		++i;
+	if (function[i] == '+' || function[i] == '-' || function[i] == '\0')
+		error("Parsing error");
+	while (function[i] >= '0' && function[i] <= '9')
+		++i;
+	if (function[i] == 'x')
+	{
+		++i;
+		if (function[i] == '^')
+		{
+			++i;
+			if (function[i] < '0' || function[i] > '9')
+				error("Parsing error");
+			while (function[i] >= '0' && function[i] <= '9')
+				++i;
+		}
+	}
+	if (function[i] < '0' || function[i] > '9')
+		if (function[i] != '-' && function[i] != '+' && function[i] != '\0')
+			error("Parsing error");
+	return (i);
+}
+
+void			parse_input(char *function, t_calc *info)
+{
+	int			i;
+
+	if (ft_strchr(function, 'x') == NULL)
+		error("The function is written in incorrect form");
+	i = -1;
+	while (function[++i])
+		if (!((function[i] >= '0' && function[i] <= '9') || \
+			function[i] == '+' || function[i] == '-' || \
+			function[i] == '^' || function[i] == 'x'))
+			error("The function caontains unrecognized symbols");
+	i = 0;
+	while (function[i])
+		i = iterate_with_index(function, i);
+	parse_function(function, info);
 }
