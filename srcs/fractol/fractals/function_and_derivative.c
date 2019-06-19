6c0eab167bd45c157196636dd39d1b08bb502222
Kirill Naumov <knaumov@e1z2r7p39.42.us.org>
2019-06-19 14:27:50 -0700

    123

--- srcs/fractol/fractals/function_and_derivative.c
+++ srcs/fractol/fractals/function_and_derivative.c
@@ -2,9 +2,9 @@
 
 COMPLEX		function(COMPLEX z)
 {
-	return (4 * z * z * z * z * z * z * z * z + 42 * z * z - 3);
+	return (5 * z * z * z - 1);
 }
 COMPLEX		derivative(COMPLEX z)
 {
-	return (32 * z * z * z * z * z * z * z + 84 * z);
+	return (15 * z * z);
 }
