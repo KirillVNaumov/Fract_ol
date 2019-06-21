b0e8dafd2cc575cfb77ba655ad5a814c7168b9a2
Allen Melikian <allenwem@gmail.com>
2019-06-21 14:22:37 -0700

    norm fixes

--- srcs/fractol/fractals/function_and_derivative.c
+++ srcs/fractol/fractals/function_and_derivative.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: amelikia <amelikia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/21 14:20:08 by amelikia          #+#    #+#             */
-/*   Updated: 2019/06/21 14:20:11 by amelikia         ###   ########.fr       */
+/*   Created: 2019/06/21 14:21:40 by amelikia          #+#    #+#             */
+/*   Updated: 2019/06/21 14:21:43 by amelikia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 COMPLEX		function(COMPLEX z)
 {
-	return (2 * z * z * z);
+	return (2 * z * z * z + 8);
 }
 
 COMPLEX		derivative(COMPLEX z)
--- utils/struct_operations/t_summand.c
+++ utils/struct_operations/t_summand.c
@@ -6,7 +6,7 @@
 /*   By: amelikia <amelikia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/19 17:16:45 by amelikia          #+#    #+#             */
-/*   Updated: 2019/06/21 14:19:09 by amelikia         ###   ########.fr       */
+/*   Updated: 2019/06/21 14:22:18 by amelikia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ t_summand	*add_summand(t_summand *list, int coefficient, int power)
 	return (list);
 }
 
-void			clear_summand(t_summand *list)
+void		clear_summand(t_summand *list)
 {
 	if (list->next)
 		clear_summand(list->next);
